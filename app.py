74bcfab8faa15dfaff8e45fda9192a615cd35f09
RickMelendez <rickmelendez001@gmail.com>
2024-06-13 21:41:33 -0400

    test

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-from flask import Flask
-from api.user_api import user_api
-from api.place_api import place_api
-from api.review_api import review_api
-from api.amenity_api import amenity_api
-from api.country_api import country_api
-from api.city_api import city_api
-from persistence.data_manager import DataManager
+from flask import Flask #type: ignore
+from API.api import route_manager
 
-def create_app():
-    app = Flask(__name__)
-    data_manager = DataManager()
 
-    # Passing the data_manager to the API blueprints
-    app.register_blueprint(user_api(data_manager), url_prefix='/api/users')
-    app.register_blueprint(place_api(data_manager), url_prefix='/api/places')
-    app.register_blueprint(review_api(data_manager), url_prefix='/api/reviews')
-    app.register_blueprint(amenity_api(data_manager), url_prefix='/api/amenities')
-    app.register_blueprint(country_api(data_manager), url_prefix='/api/countries')
-    app.register_blueprint(city_api(data_manager), url_prefix='/api/cities')
-
-    return app
+app = Flask(__name__)
+route_manager(app)
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
