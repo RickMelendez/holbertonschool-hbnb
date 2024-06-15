7dc9249a711e1b8d29e677658036de3eb202a897
RickMelendez <rickmelendez001@gmail.com>
2024-06-15 17:19:53 -0400

    test

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-from flask import Flask #type: ignore
-from API.api import route_manager
+import sys
+import os
+from flask import Flask
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'API')))
+
+from api import route_manager
 
 app = Flask(__name__)
 route_manager(app)
