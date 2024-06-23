1c74866253c78637f53086dc22186bf0a5bf6b91
RickMelendez <rickmelendez001@gmail.com>
2024-06-22 22:15:48 -0400

    hot fix

--- Models/review.py
+++ Models/review.py
@@ -1,9 +1,19 @@
 from .base_model import BaseModel
 
 class Review(BaseModel):
-    def __init__(self, place_id="", user_id="", text="", comment="", *args, **kwargs):
+    def __init__(self, place_id="", user_id="", text="", comment="", **kwargs):
+        super().__init__(**kwargs)
         self.place_id = place_id
         self.user_id = user_id
         self.text = text
         self.comment = comment
-        super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        data = super().to_dict()
+        data.update({
+            'place_id': self.place_id,
+            'user_id': self.user_id,
+            'text': self.text,
+            'comment': self.comment
+        })
+        return data
